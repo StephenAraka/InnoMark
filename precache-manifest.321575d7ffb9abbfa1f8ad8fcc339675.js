f1a9abedf9f82049cb62b6a1cd66a03e6940fa80
StephenAraka <stevenaraka@gmail.com>
2019-11-27 17:35:21 +0300

    Updates

---
+++ precache-manifest.321575d7ffb9abbfa1f8ad8fcc339675.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e0c875b563b6a77ae0ecec77996d6b",
+    "url": "/innomark/index.html"
+  },
+  {
+    "revision": "7a5f37999290048e134e",
+    "url": "/innomark/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "45eea968b046bce15ada",
+    "url": "/innomark/static/css/main.2c8e7a25.chunk.css"
+  },
+  {
+    "revision": "7a5f37999290048e134e",
+    "url": "/innomark/static/js/2.0ddae5f9.chunk.js"
+  },
+  {
+    "revision": "45eea968b046bce15ada",
+    "url": "/innomark/static/js/main.918a9c9b.chunk.js"
+  },
+  {
+    "revision": "f303c4e58e09ec38ccb2",
+    "url": "/innomark/static/js/runtime-main.b6aa8517.js"
+  },
+  {
+    "revision": "a5c12e743790fe179b0344e753116619",
+    "url": "/innomark/static/media/agric.a5c12e74.jpg"
+  },
+  {
+    "revision": "52b951c3a14829735c9032245d262d8e",
+    "url": "/innomark/static/media/agric2.52b951c3.jpg"
+  },
+  {
+    "revision": "edec49c064e770f8b01109d260da568f",
+    "url": "/innomark/static/media/agric2.edec49c0.jpg"
+  },
+  {
+    "revision": "00c3b26f52ca2c4279d1a08454f6b7d0",
+    "url": "/innomark/static/media/agric3.00c3b26f.jpg"
+  },
+  {
+    "revision": "bbb99940a2a98b5221addc53c2cf2fda",
+    "url": "/innomark/static/media/agric3.bbb99940.jpg"
+  },
+  {
+    "revision": "977dbd81002023be38f05cd91d9b6132",
+    "url": "/innomark/static/media/agric4.977dbd81.jpg"
+  },
+  {
+    "revision": "f21f57e9182a35f872055ab94e054dd3",
+    "url": "/innomark/static/media/agric4.f21f57e9.jpeg"
+  },
+  {
+    "revision": "ab066326ca830ad638207426c6534ea2",
+    "url": "/innomark/static/media/agricField.ab066326.jpg"
+  },
+  {
+    "revision": "8a4b8eb5936d2a56c5978306a6e2e3ce",
+    "url": "/innomark/static/media/agricSUre.8a4b8eb5.jpg"
+  },
+  {
+    "revision": "e6d23188819f4ef53fa29e8072186444",
+    "url": "/innomark/static/media/agricSureDevice.e6d23188.png"
+  },
+  {
+    "revision": "20593f506446bfa4a7e1be55fb948d11",
+    "url": "/innomark/static/media/agricfield.20593f50.jpg"
+  },
+  {
+    "revision": "b748425eac0d70f35b623a066b4f4cd2",
+    "url": "/innomark/static/media/agricfield2.b748425e.jpg"
+  },
+  {
+    "revision": "c8cdebf270b94f07de8625d3273974e0",
+    "url": "/innomark/static/media/educ.c8cdebf2.jpeg"
+  },
+  {
+    "revision": "f15389de1dee0f30d870b99cad43cac6",
+    "url": "/innomark/static/media/educ2.f15389de.jpg"
+  },
+  {
+    "revision": "962bafe1bafec79347e7ced62eaeff1c",
+    "url": "/innomark/static/media/finance2.962bafe1.jpg"
+  },
+  {
+    "revision": "bce1a20d0f8463bd39fe36361f38688e",
+    "url": "/innomark/static/media/health.bce1a20d.jpg"
+  },
+  {
+    "revision": "07af06feade9101a2f229cc6c08677b9",
+    "url": "/innomark/static/media/kyu.07af06fe.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/innomark/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "238af16d0235854c9c9d5026ba897a8c",
+    "url": "/innomark/static/media/mak.238af16d.png"
+  },
+  {
+    "revision": "619b019a6ba44bf212c023037af13983",
+    "url": "/innomark/static/media/ndejje.619b019a.png"
+  },
+  {
+    "revision": "2017dfd18dec1d68f7bd727e5a81fc1c",
+    "url": "/innomark/static/media/phoneapp.2017dfd1.jpg"
+  },
+  {
+    "revision": "cb0a022ef1f5f82ab5ab28f634ca2d18",
+    "url": "/innomark/static/media/phoneapp2.cb0a022e.png"
+  },
+  {
+    "revision": "26e0c3b2c05a81575a023ed07ee49997",
+    "url": "/innomark/static/media/phoneapp3.26e0c3b2.jpg"
+  },
+  {
+    "revision": "ef90fc30bd3c556652d09033564143a3",
+    "url": "/innomark/static/media/random.ef90fc30.png"
+  },
+  {
+    "revision": "e04a24fa0ad791a942fb2e3189198e29",
+    "url": "/innomark/static/media/vu.e04a24fa.png"
+  }
+]);
